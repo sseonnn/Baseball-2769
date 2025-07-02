c31a9bcada08ebda617f9f363c929c947114c05a
jeong.lee <com2769@gmail.com>
2025-07-02 14:02:27 +0900

    [feature] 52p_1

--- BaseballKATA/baseball_test.cpp
+++ BaseballKATA/baseball_test.cpp
@@ -29,6 +29,14 @@ TEST_F(BaseballFixture, ReturnSolveResultIfMatchedNumber) {
 	EXPECT_EQ(0, result.balls);
 }
 
+TEST_F(BaseballFixture, Return2S0B) {
+	GuessResult result = game.guess("124");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(2, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
